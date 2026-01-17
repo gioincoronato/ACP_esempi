6d52514e1d9dbf5068084dcff737a18f3b9fb9a7
Giovanni Incoronato <incoronato.giovanni.1998@gmail.com>
2026-01-17 15:50:51 +0100

    aggiunto es 2

---
+++ ACP_es2.py
@@ -0,0 +1,13 @@
+# Classi
+
+
+class AutoMobile:
+
+    def __init__(self, marca, modello, km_iniziali = 0):
+        var_test = "ciao"
+        self.marca = marca
+        self.modello = modello
+        self.Km = km_iniziali
+        self.accesa = False
+
+    pass
