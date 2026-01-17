2537d61a8920c29962ed585a4e9aa83a8ca71b55
Giovanni Incoronato <incoronato.giovanni.1998@gmail.com>
2026-01-17 15:55:49 +0100

    esercizio 2

--- ACP_es2.py
+++ ACP_es2.py
@@ -4,7 +4,9 @@
 class AutoMobile:
 
     def __init__(self, marca, modello, km_iniziali = 0):
-        var_test = "ciao"
+        var_test = "ciao" #variabile locale del metodo init
+        # attributi dell'istanza
+        # self serve ad accedere allo stato interno dell'oggetto
         self.marca = marca
         self.modello = modello
         self.Km = km_iniziali
