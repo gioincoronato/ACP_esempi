b86940c02108c5dbcc9a164e7b8d8e54c848b76c
Giovanni Incoronato <incoronato.giovanni.1998@gmail.com>
2026-01-20 15:55:32 +0100

    esempio thread con classe

---
+++ Class_thread_es.py
@@ -0,0 +1,30 @@
+import threading
+from time import sleep
+
+
+
+
+class calculatorThread(threading.Thread):
+    def __init__(self, id_operazione, valore):
+        
+        super().__init__() # chiamo il costruttore della classe base
+        self.id = id_operazione
+        self.valore = valore
+
+    # per eseguire le operazioni faccio l'overide del metodo run()
+    def run(self):
+        print(f"Thread {self.id} avvio calcolo con il valore {self.valore}... ")
+        sleep(3)
+        risultato = self.valore * self.valore
+        print(f"risultato dell'operazione: {risultato}")
+
+
+if __name__ == "__main__":
+   t1 = calculatorThread(id_operazione=1, valore=10)
+   t2 = calculatorThread(id_operazione=2, valore=20)
+
+   t1.start()
+   t2.start()
+
+   t1.join()
+   t2.join()
